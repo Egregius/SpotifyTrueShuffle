335d42cc278fd0e02d8febeaa7cb8941479b972f
Guy Verschuere <guy@egregius.be>
2023-09-04 02:32:51 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ $clientsecrect='cb93c4b50f8c41e6acb08ab97f74c351';
 $returnurl='http://192.168.2.20/spotify';
 $telegrambot='1939143936:AAFX4gPC6szW__P2eQvLR_GBwaExRRp1D3A';
 $telegramchatid=964051278;
+$spotifyusername='egregiusspotify';
