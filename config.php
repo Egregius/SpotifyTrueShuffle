629c1fb854b725c60e466410d5a8d3b321fc461a
Guy Verschuere <guy@egregius.be>
2023-09-04 02:42:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,5 @@
 $clientid='9addf558717e406caebc2a48c236a93e';
 $clientsecrect='cb93c4b50f8c41e6acb08ab97f74c351';
 $returnurl='http://192.168.2.20/spotify';
-$telegrambot='1939143936:AAFX4gPC6szW__P2eQvLR_GBwaExRRp1D3A';
-$telegramchatid=964051278;
 $spotifyusername='egregiusspotify';
-$randomposition=0.8;
+$randomposition=0.8;// Tracks are placed after this part of the playlist. 0.8 places the tracks in the last 20%. Must be number smaller than 1.
