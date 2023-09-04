3577a664c04ff3dfda8ed546a5100d00b57d927a
Guy Verschuere <guy@egregius.be>
2023-09-04 02:13:39 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$clientid='9addf558717e406caebc2a48c236a93e';
+$clientsecrect='cb93c4b50f8c41e6acb08ab97f74c351';
+$returnurl='http://192.168.2.20/spotify';
+$telegrambot='1939143936:AAFX4gPC6szW__P2eQvLR_GBwaExRRp1D3A';
+$telegramchatid=964051278;
